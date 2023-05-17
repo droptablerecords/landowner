13df153bb3cd0b28836996e266be4c6ff53c908a
Allen Smith <loranallensmith@gmail.com>
2023-05-17 12:26:05 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='landowner',
+    version='0.1.0',
+    description='A Python package for working with export files from the big social media platforms.',
+    long_description=readme,
+    author='Loran Allen Smith',
+    author_email='hi@loranallensmith.me',
+    url='https://github.com/loranallensmith/landowner',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
